67ce2a7b214570c5755707b5540469df0c3f3235
lootan_h <loosung@naver.com>
2024-08-28 15:27:46 +0900

    Random Color Library Implementation

--- section03/src/index.js
+++ section03/src/index.js
@@ -1,9 +1,9 @@
 // Common JS (CJS)
 // const { add, sub } = require("./math");
 
-import mulp, { add, sub } from "./math.js";
-
 // ES Module System
+import mulp, { add, sub } from "./math.js";
+import randomColor from "randomcolor";
 
 const addNum = add(1, 2);
 const sunNum = sub(1, 2);
@@ -12,3 +12,6 @@ const mulNum = mulp(1, 2);
 console.log(addNum);
 console.log(sunNum);
 console.log(mulNum);
+
+const color = randomColor();
+console.log(color);
