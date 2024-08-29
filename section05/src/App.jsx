5758baac87ad9e51be37202c0b886ed5bfba783b
lootan_h <loosung@naver.com>
2024-08-29 13:55:13 +0900

    state를 props로 자식 컴포넌트에 보내기

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,33 +1,55 @@
 import './App.css';
 import { useState } from 'react';
 
-// 부모 Component
-function App() {
-    const [count, setCount] = useState(0);
+// Re-Rendering 되는 경우
+// 1. state가 변경 될때
+// 2. probs가 변경 될때
+// 3. 부모 Componet가 변경 될때
+
+// 자식 Componet
+const Bulb = () => {
     const [light, setLight] = useState('OFF');
+    return (
+        <div>
+            {light === 'ON' ? (
+                <h1 style={{ backgroundColor: 'orange' }}>ON</h1>
+            ) : (
+                <h1 style={{ backgroundColor: 'gray' }}>OFF</h1>
+            )}
+            <button
+                onClick={() => {
+                    setLight(light === 'ON' ? 'OFF' : 'ON');
+                }}
+            >
+                {light === 'ON' ? '끄기' : '켜기'}
+            </button>
+        </div>
+    );
+};
+
+// 자식 2 Component
+const Counter = () => {
+    const [count, setCount] = useState(0);
 
     return (
+        <div>
+            <h1>{count}</h1>
+            <button>
+                onClinc
+                {() => {
+                    setCount(count + 1);
+                }}
+            </button>
+        </div>
+    );
+};
+
+// 부모 Component
+function App() {
+    return (
         <>
-            <div>
-                <h1>{count}</h1>
-                <button
-                    onClick={() => {
-                        setCount(count + 1);
-                    }}
-                >
-                    +
-                </button>
-            </div>
-            <div>
-                <h1>{light}</h1>
-                <button
-                    onClick={() => {
-                        setLight(light === 'ON' ? 'OFF' : 'ON');
-                    }}
-                >
-                    {light === 'ON' ? '끄기' : '켜기'}
-                </button>
-            </div>
+            <Bulb />
+            <Counter />
         </>
     );
 }
