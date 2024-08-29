5609372d8ac0626e17b82f25cff89adec4623532
lootan_h <loosung@naver.com>
2024-08-29 12:57:34 +0900

    Event Handling

--- section05/src/components/Button.jsx
+++ section05/src/components/Button.jsx
@@ -1,7 +1,13 @@
 // Props는 부모에서 자식으로 밖에 줄 수 없다.
 const Button = ({ text, color, children }) => {
+    // 이벤트 함수,
+    // 객체 (Syntheic Base Event - 합성 이벤트) -> 모든 브라우저의 이벤트 객체를 하나로 통일한 형태
+    const onClickBtn = (event) => {
+        event;
+    };
+
     return (
-        <button style={{ color: color }}>
+        <button onClick={onClickBtn} style={{ color: color }}>
             {text} - {color.toUpperCase()}
             {children}
         </button>
