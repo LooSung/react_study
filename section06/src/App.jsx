d2ccbeccde49b7987c6f2459c2c7e96eaeccd9b8
lootan_h <loosung@naver.com>
2024-08-30 10:32:11 +0900

    부모와 자식 간의 컨포넌트는 이동이 가능하지만, 자식 간의 Reference를 이동이 불가능하다.

--- section06/src/App.jsx
+++ section06/src/App.jsx
@@ -3,15 +3,23 @@ import './App.css';
 import Viewer from './components/Viewer';
 import Controller from './components/Controller';
 
+import { useState } from 'react';
+
 function App() {
+    const [count, setCounter] = useState(0);
+
+    const onClickBtn = (value) => {
+        setCounter(count + value);
+    };
+
     return (
         <div className='App'>
             <h1> Simple Counter</h1>
             <section>
-                <Viewer />
+                <Viewer count={count} />
             </section>
             <section>
-                <Controller />
+                <Controller onClickBtn={onClickBtn} />
             </section>
         </div>
     );
