459e08bbd2d55c91d9a03269a59834019ab781cc
lootan_h <loosung@naver.com>
2024-09-02 13:15:38 +0900

    Refactor state to reducer

--- section09/src/App.jsx
+++ section09/src/App.jsx
@@ -1,11 +1,10 @@
 import './App.css';
 
-import { useState, useRef } from 'react';
+import { useState, useRef, useReducer } from 'react';
 
 import Header from './components/Header';
 import Editor from './components/Editor';
 import List from './components/List';
-import Exam from './components/Exam';
 
 const mockData = [
     {
@@ -28,55 +27,59 @@ const mockData = [
     },
 ];
 
+function reducer(state, action) {
+    switch (action.type) {
+        case 'CREATE':
+            return [action.data, ...state];
+        case 'UPDATE':
+            return state.map((item) =>
+                item.id === action.data.targetId
+                    ? { ...item, isDone: !item.isDone }
+                    : item
+            );
+        case 'DELETE':
+            return state.filter((item) => item.id !== action.data.targetId);
+        default:
+            return state;
+    }
+}
+
 function App() {
-    const [todos, setTodos] = useState(mockData);
+    const [todos, dispatch] = useReducer(reducer, mockData);
     const idRef = useRef(3);
 
     const onCreate = (content) => {
-        const newTodo = {
-            id: idRef.current++,
-            isDone: false,
-            content: content,
-            date: new Date().getTime(),
-        };
-
-        setTodos([newTodo, ...todos]);
+        dispatch({
+            type: 'CREATE',
+            data: {
+                id: idRef.current++,
+                isDone: false,
+                content: content,
+                date: new Date().getTime(),
+            },
+        });
     };
 
     const onUpdate = (targetId) => {
-        setTodos(
-            todos.map((todo) => {
-                if (todo.id === targetId) {
-                    return {
-                        ...todo,
-                        isDone: !todo.isDone,
-                    };
-                }
-
-                return todo;
-            })
-        );
+        dispatch({
+            type: 'UPDATE',
+            data: { targetId: targetId },
+        });
     };
 
-    // 3항 연산자 사용
-    // const onUpdate = (targetId) => {
-    //     setTodos(
-    //         todos.map((todo) => {
-    //             todo.id == targetId ? { ...todo, isDone: !todo.isDone } : todo;
-    //         })
-    //     );
-    // };
-
     const onDelete = (targetId) => {
-        setTodos(todos.filter((todo) => todo.id !== targetId));
+        dispatch({
+            type: 'DELETE',
+            data: { targetId: targetId },
+        });
     };
 
     return (
         <div className='App'>
-            <Exam />
-            {/* <Header />
+            {/* /<Exam /> */}
+            <Header />
             <Editor onCreate={onCreate} />
-            <List todos={todos} onUpdate={onUpdate} onDelete={onDelete} /> */}
+            <List todos={todos} onUpdate={onUpdate} onDelete={onDelete} />
         </div>
     );
 }
