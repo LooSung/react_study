1ce870fcbcae79efa42d5cb0c2878ff0e4096ba3
lootan_h <loosung@naver.com>
2024-09-02 11:38:10 +0900

    Todo-List 만들기

---
+++ section08/src/components/Editor.jsx
@@ -0,0 +1,42 @@
+import './Editor.css';
+import { useState, useRef } from 'react';
+
+const Editor = ({ onCreate }) => {
+    const [content, setContent] = useState('');
+    const contentRef = useRef();
+
+    const onChangeContent = (event) => {
+        setContent(event.target.value);
+    };
+
+    const onSubmit = () => {
+        if (content == '') {
+            contentRef.current.focus();
+            return;
+        }
+
+        onCreate(content);
+        setContent('');
+    };
+
+    const onKeyDown = (event) => {
+        if (event.keycode === 13) {
+            onSubmit();
+        }
+    };
+
+    return (
+        <div className='Editor'>
+            <input
+                ref={contentRef}
+                value={content}
+                onChange={onChangeContent}
+                onKeyDown={onKeyDown}
+                placeholder='새로운 Todo...'
+            ></input>
+            <button onClick={onSubmit()}>Add</button>
+        </div>
+    );
+};
+
+export default Editor;
