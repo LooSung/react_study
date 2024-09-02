3b97224afd50572916633566049576dd068cc72c
lootan_h <loosung@naver.com>
2024-09-02 16:57:16 +0900

    useMemo : Memorization -> 반복되는 연산값을 메모리에 저장한 다음에 사용한다.

--- section10/src/components/List.jsx
+++ section10/src/components/List.jsx
@@ -1,10 +1,27 @@
 import './List.css';
 import TodoItem from './TodoItem';
 
-import { useState } from 'react';
+import { useState, useMemo } from 'react';
 
 const List = ({ todos, onUpdate, onDelete }) => {
     const [search, setSearch] = useState('');
+
+    // Memorization -> 의존성배열 : deps (이 Count연산은 1번만 실행되게 만들었다.)
+    // 구조분해 할당해서 어떤 데이터가 Memory에 저장 될 것인지 선정
+    const { totalCount, doneCount, notDoneCount } = useMemo(() => {
+        const totalCount = todos.length;
+        const doneCount = todos.filter((todo) => {
+            todo.isDone;
+        }).length;
+        const notDoneCount = totalCount - doneCount;
+
+        return {
+            totalCount,
+            doneCount,
+            notDoneCount,
+        };
+    }, [todos]);
+    // 값을 가져올 것에 대한 설정.
 
     const onChangeSearch = (event) => {
         setSearch(event.target.value);
@@ -25,6 +42,11 @@ const List = ({ todos, onUpdate, onDelete }) => {
     return (
         <div className='List'>
             <h4>TO-DO List 🥸</h4>
+            <div>
+                <div>{totalCount}</div>
+                <div>{doneCount}</div>
+                <div>{notDoneCount}</div>
+            </div>
             <input
                 value={search}
                 onChange={onChangeSearch}
