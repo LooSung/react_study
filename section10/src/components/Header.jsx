d371a1b8a2f3f1f9a45b5674a044265c1726159b
lootan_h <loosung@naver.com>
2024-09-02 17:11:59 +0900

    React.memo -> 컴포넌트의 인수로 받아 최적화된 컴포넌트로 만들어 반환 :
    부모한테 받은 props가 아닌이상, 리렌더링 되지 않는다.
    
    하지만 memo는 얕은비교 (===)를 하기 때문에 객체 타입({}, [])의 값은 props가 바뀌었다 판단 할 수 도 있다.
    
    참고 : https://patterns-dev-kr.github.io/design-patterns/hoc-pattern/

--- section10/src/components/Header.jsx
+++ section10/src/components/Header.jsx
@@ -1,4 +1,5 @@
 import './Header.css';
+import { memo } from 'react';
 
 const Header = () => {
     return (
@@ -9,4 +10,8 @@ const Header = () => {
     );
 };
 
-export default Header;
+// 자신이 받은 Props가 바뀌지 않으면, Re-Rendering 되지 않는다.
+// const memoizedHeader = memo(Header);
+// export default memoizedHeader;
+
+export default memo(Header);
