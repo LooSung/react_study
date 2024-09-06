37089475b7c09e833c3e9f53516722d8305c34d9
lootan_h <loosung@naver.com>
2024-09-06 12:54:50 +0900

    공통 컴포넌트 (Header, Button)

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -4,6 +4,8 @@ import Home from './pages/Home';
 import New from './pages/New';
 import Diary from './pages/Diary';
 import Notfound from './pages/Notfound';
+import Button from './components/Button';
+import Header from './components/Header';
 
 import { getEmotionImage } from './util/get-emotion-images';
 
@@ -34,16 +36,15 @@ function App() {
 
     return (
         <>
-            <img src={getEmotionImage(1)} />
-            <img src={getEmotionImage(2)} />
-            <img src={getEmotionImage(3)} />
-            <img src={getEmotionImage(4)} />
-            <img src={getEmotionImage(5)} />
-            <div>
-                <Link to={'/'}>Home</Link>
-                <Link to={'/new'}>New</Link>
-                <Link to={'/diary'}>Diary</Link>
-            </div>
+            <Header
+                title={'Header'}
+                leftChild={<Button text={'Left'} />}
+                rightChild={<Button text={'Right'} />}
+            />
+            <Button text={''} onClick={() => {}} />
+            <Button text={''} type={'POSITIVE'} onClick={() => {}} />
+            <Button text={''} type={'NEGATIVE'} onClick={() => {}} />
+
             <Routes>
                 <Route path='/' element={<Home />}></Route>
                 <Route path='/new' element={<New />}></Route>
