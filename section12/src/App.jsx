4c503ca3a4333047fdff6f5eb659a7de2e1a2ed3
lootan_h <loosung@naver.com>
2024-09-06 13:21:39 +0900

    Daily Data 관리 기능 구현 2
    1. useReducer Hook을 사용한 Create, Udpate Delete 기능 제어 구현
    (이전 To-Do List와 같이 구현)
    
    2. useRef Hook을 사용한 자동증가 ID 구현
    
    3. DiaryStateContext, DiartDispatchContext 를 사용해서 전역으로 사용 할 수있게 설정

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { useReducer } from 'react';
+import { useReducer, useRef, createContext } from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
@@ -7,9 +7,6 @@ import New from './pages/New';
 import Diary from './pages/Diary';
 import Edit from './pages/Edit';
 import Notfound from './pages/Notfound';
-
-import Button from './components/Button';
-import Header from './components/Header';
 
 // 1. "/" 모든 일기를 조회하는 Home Page
 // 2. "/new" : 새로운 일기를 작성하는 New Page
@@ -45,21 +42,81 @@ const mockData = [
 ];
 
 function reducer(state, action) {
-    return state;
+    switch (action.type) {
+        case 'CREATE':
+            return [data, ...state];
+        case 'UPDATE':
+            return state.map((item) =>
+                String(item.id) === String(action.data.id) ? action.data : data
+            );
+        case 'DELETE':
+            return state.filter(
+                (item) => Stirng(item.id) !== String(action.data.id)
+            );
+        default:
+            return;
+    }
 }
+
+const DiaryStateContext = createContext();
+const DiartDispatchContext = createContext();
 
 function App() {
     const [data, dispatch] = useReducer(reducer, mockData);
+    const idRef = useRef(3);
+
+    const onCreate = (createdDate, emotionId, content) => {
+        dispatch({
+            type: 'CREATE',
+            data: {
+                id: idRef.current++,
+                createdDate,
+                emotionId,
+                content,
+            },
+        });
+    };
+
+    const onUpdate = (id, createdDate, emotionId, content) => {
+        dispatch({
+            type: 'UPDATE',
+            data: {
+                id,
+                createdDate,
+                emotionId,
+                content,
+            },
+        });
+    };
+
+    const onDelete = (id) => {
+        dispatch({
+            type: 'DELTE',
+            data: {
+                id,
+            },
+        });
+    };
 
     return (
         <>
-            <Routes>
-                <Route path='/' element={<Home />}></Route>
-                <Route path='/new' element={<New />}></Route>
-                <Route path='/diary/:id' element={<Diary />}></Route>
-                <Route path='/edit/:id' element={<Edit />}></Route>
-                <Route path='*' element={<Notfound />}></Route>
-            </Routes>
+            <DiaryStateContext.Provider value={data}>
+                <DiartDispatchContext.Provider
+                    value={{
+                        onCreate,
+                        onUpdate,
+                        onDelete,
+                    }}
+                >
+                    <Routes>
+                        <Route path='/' element={<Home />}></Route>
+                        <Route path='/new' element={<New />}></Route>
+                        <Route path='/diary/:id' element={<Diary />}></Route>
+                        <Route path='/edit/:id' element={<Edit />}></Route>
+                        <Route path='*' element={<Notfound />}></Route>
+                    </Routes>
+                </DiartDispatchContext.Provider>
+            </DiaryStateContext.Provider>
         </>
     );
 }
