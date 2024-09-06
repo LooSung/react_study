b287d01c050fda702546940db25e1f85be6fd7f1
lootan_h <loosung@naver.com>
2024-09-06 13:04:15 +0900

    Daily Data 관리 기능 구현 1
    * reducer를 사용하는 메뉴간의 전역 State 설정

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -1,17 +1,20 @@
 import './App.css';
-import { Routes, Route, Link, useNavigate } from 'react-router-dom';
+import { useReducer } from 'react';
+import { Routes, Route } from 'react-router-dom';
+
 import Home from './pages/Home';
 import New from './pages/New';
 import Diary from './pages/Diary';
+import Edit from './pages/Edit';
 import Notfound from './pages/Notfound';
+
 import Button from './components/Button';
 import Header from './components/Header';
-
-import { getEmotionImage } from './util/get-emotion-images';
 
 // 1. "/" 모든 일기를 조회하는 Home Page
 // 2. "/new" : 새로운 일기를 작성하는 New Page
 // 3. "/diary" : 일기장을 상세히 조회하는 Diary Page
+// 4. "/edit" : 일기장을 수정하는 Edit Page
 
 // 1. URL Parameter
 // import { useSearchParams } from 'react-router-dom';
@@ -26,29 +29,35 @@ import { getEmotionImage } from './util/get-emotion-images';
 // div>{params.id}번 일기입니다.</div>;
 // Client : "https://localhost/etc/1"
 
-function App() {
-    const nav = useNavigate();
+const mockData = [
+    {
+        id: 1,
+        createdDate: new Date().getTime,
+        emotionId: 1,
+        content: '내용이다.',
+    },
+    {
+        id: 2,
+        createdDate: new Date().getTime,
+        emotionId: 2,
+        content: '내용이다. 2',
+    },
+];
 
-    // 버튼을 이용해서 특정 페이지로 이동을 해야한다면 Navigate를 이용.
-    const onClickButton = () => {
-        nav('/new');
-    };
+function reducer(state, action) {
+    return state;
+}
+
+function App() {
+    const [data, dispatch] = useReducer(reducer, mockData);
 
     return (
         <>
-            <Header
-                title={'Header'}
-                leftChild={<Button text={'Left'} />}
-                rightChild={<Button text={'Right'} />}
-            />
-            <Button text={''} onClick={() => {}} />
-            <Button text={''} type={'POSITIVE'} onClick={() => {}} />
-            <Button text={''} type={'NEGATIVE'} onClick={() => {}} />
-
             <Routes>
                 <Route path='/' element={<Home />}></Route>
                 <Route path='/new' element={<New />}></Route>
                 <Route path='/diary/:id' element={<Diary />}></Route>
+                <Route path='/edit/:id' element={<Edit />}></Route>
                 <Route path='*' element={<Notfound />}></Route>
             </Routes>
         </>
