efb26a6afce3d073dd7fc6abd2f31e55581d0776
lootan_h <loosung@naver.com>
2024-09-06 11:46:21 +0900

    Init & Route 기능 추가

---
+++ section12/src/App.jsx
@@ -0,0 +1,50 @@
+import './App.css';
+import { Routes, Route, Link, useNavigate } from 'react-router-dom';
+import Home from './pages/Home';
+import New from './pages/New';
+import Diary from './pages/Diary';
+import Notfound from './pages/Notfound';
+
+// 1. "/" 모든 일기를 조회하는 Home Page
+// 2. "/new" : 새로운 일기를 작성하는 New Page
+// 3. "/diary" : 일기장을 상세히 조회하는 Diary Page
+
+// 1. URL Parameter
+// import { useSearchParams } from 'react-router-dom';
+// const [params, setParams] = useSearchParams();
+// parms.get("value")
+// Client : "https://localhost/etc/?value=hello"
+
+// 2. Query String ->
+// <Route path='/diary/:id' element={<Diary />}></Route>
+// import { useParams } from 'react-router-dom';
+// const params = useParams();
+// div>{params.id}번 일기입니다.</div>;
+// Client : "https://localhost/etc/1"
+
+function App() {
+    const nav = useNavigate();
+
+    // 버튼을 이용해서 특정 페이지로 이동을 해야한다면 Navigate를 이용.
+    const onClickButton = () => {
+        nav('/new');
+    };
+
+    return (
+        <>
+            <div>
+                <Link to={'/'}>Home</Link>
+                <Link to={'/new'}>New</Link>
+                <Link to={'/diary'}>Diary</Link>
+            </div>
+            <Routes>
+                <Route path='/' element={<Home />}></Route>
+                <Route path='/new' element={<New />}></Route>
+                <Route path='/diary/:id' element={<Diary />}></Route>
+                <Route path='*' element={<Notfound />}></Route>
+            </Routes>
+        </>
+    );
+}
+
+export default App;
---
+++ section12/src/pages/Notfound.jsx
@@ -0,0 +1,5 @@
+const Notfound = () => {
+    return <div>This page not found.</div>;
+};
+
+export default Notfound;
